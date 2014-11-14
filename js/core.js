7593adcade27c7f22481d0d6d9a607a8977766d9
Byrdann Fox <mobileupstate@gmail.com>
2014-11-14 12:46:41 -0500

    Updates...

--- js/core.js
+++ js/core.js
@@ -1,30 +1,39 @@
-
-        // CONFIG. A FUNCTION THAT TAKES IN THE PROPER NEEDED ARGS...
+Keen.ready(function() {
 
         var averageSentenceLength = new Keen.Query("average", {
             eventCollection: "sentence_spoken",
             targetProperty: "wordsPerSentence",
-            timeframe: "this_week",
-            interval: "daily"
+            timeframe: "this_30_minutes",
+            interval: "minutely"
         });
 
         client.draw(averageSentenceLength, document.getElementById("average-sentence-length"), {
-            chartType: "barchart",
+            chartType: "columnchart",
             title: "Average Sentence Length"
         });
 
-        /*var sentenceCount = new Keen.Query("sum", {
+        var sentenceCount = new Keen.Query("sum", {
             eventCollection: "sentence_spoken",
             targetProperty: "sentences",
-            timeframe: "this_week",
-            interval: "daily"
+            timeframe: "this_30_minutes",
+            interval: "minutely"
+        });
+
+        client.draw(sentenceCount, document.getElementById("sentence-count"), {
+            chartType: "areachart",
+            title: "Sentence Count"
         });
 
         var averageWordLength = new Keen.Query("average", {
             eventCollection: "sentence_spoken",
             targetProperty: "averageWordLength",
-            timeframe: "this_week",
+            timeframe: "this_month",
             interval: "daily"
+        });
+
+        client.draw(averageWordLength, document.getElementById("average-word-length"), {
+            chartType: "linechart",
+            title: "Average Word Length"
         });
 
         var wordCount = new Keen.Query("sum", {
@@ -32,19 +41,11 @@
             targetProperty: "wordsCount",
             timeframe: "this_week",
             interval: "daily"
-        });*/
-
-        /*client.draw(sentenceCount, document.getElementById("sentence-count"), {
-            chartType: "areachart",
-            title: "Sentence Count"
-        });
-
-        client.draw(averageWordLength, document.getElementById("average-word-length"), {
-            chartType: "barchart",
-            title: "Average Word Length"
         });
 
         client.draw(wordCount, document.getElementById("word-count"), {
             chartType: "areachart",
             title: "Word Count"
-        });*/
+        });
+
+});
